c75bce6f2596d953aa1f561f7ef5867831bcaea2
Johan Kristell <johan@jott.se>
2019-05-10 15:47:40 +0200

    Work

--- examples/infrared.rs
+++ examples/infrared.rs
@@ -1,5 +1,7 @@
 #![no_main]
 #![no_std]
+#![allow(deprecated)]
+#![allow(unused_imports)]
 
 //use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
@@ -21,11 +23,17 @@ const CPU_FREQ: u32 = 84_000_000;
 
 #[app(device = nucleo_f401re::hal::stm32)]
 const APP: () = {
+    // Res
+    static mut CLOCK: u32 = 0;
+
     // Late resources
     static mut EXTI: stm32::EXTI = ();
     static mut BUTTON: PC13<Input<PullDown>> = ();
     static mut LED: PA5<Output<PushPull>> = ();
-    static mut IR: IrReceiver<PB8<Input<PullDown>>> = ();
+    //static mut IR: IrReceiver<PB8<Input<PullDown>>> = ();
+
+    static mut INPIN: PB8<Input<PullDown>> = ();
+    static mut IRRECV: IrRecv = ();
 
     #[init(schedule = [sample])]
     fn init() {
@@ -35,6 +43,9 @@ const APP: () = {
         // Device specific peripherals
         let mut device: stm32::Peripherals = device;
 
+        // Enable the clock for the SYSCFG
+        device.RCC.apb2enr.modify(|_, w| w.syscfgen().enabled());
+
         // Configure PC13 (User Button) as an input
         let gpioc = device.GPIOC.split();
         let mut button = gpioc.pc13.into_pull_down_input();
@@ -42,9 +53,6 @@ const APP: () = {
         // Configure the led pin as an output
         let gpioa = device.GPIOA.split();
         let led = gpioa.pa5.into_push_pull_output();
-
-        // Enable the clock for the SYSCFG
-        device.RCC.apb2enr.modify(|_, w| w.syscfgen().enabled());
 
         // Enable interrupt on PC13
         button.make_interrupt_source(&mut device.SYSCFG);
@@ -56,9 +64,8 @@ const APP: () = {
 
         ir_pin.make_interrupt_source(&mut device.SYSCFG);
         ir_pin.enable_interrupt(&mut device.EXTI);
-        ir_pin.trigger_on_edge(&mut device.EXTI, Edge::RISING);
-
-        let recv = IrReceiver::new(ir_pin);
+        ir_pin.trigger_on_edge(&mut device.EXTI, Edge::RISING_FALLING);
+        //let recv = IrReceiver::new(ir_pin);
 
         // Setup the system clock
         let rcc = device.RCC.constrain();
@@ -73,40 +80,34 @@ const APP: () = {
         EXTI = device.EXTI;
         LED = led;
         BUTTON = button;
-        IR = recv;
+        INPIN = ir_pin;
+        IRRECV = IrRecv::new();
+        //IR = recv;
     }
 
-    #[idle(resources = [IR])]
+    #[idle]
     fn idle() -> ! {
         hprintln!("idle").unwrap();
 
         // The idle loop
-        loop {
-/*
-            if resources.IR.done {
-                hprintln!("buf: {}", resources.IR.buf);
+        loop { }
+    }
 
-                resources.IR.done = false;
-            }
-*/
+    #[task(resources = [IRRECV])]
+    fn debug_print() {
+        let ir: &IrRecv = &resources.IRRECV;
 
+        for ts in &ir.buf {
+            hprintln!("{}", ts).unwrap();
         }
     }
 
-    #[task(resources = [IR])]
-    fn debug_print() {
-        hprintln!("res: {}", resources.IR.buf).unwrap();
-    }
-
-    #[task(schedule = [sample], spawn = [debug_print], resources = [IR])]
+    #[task(schedule = [sample], spawn = [debug_print], resources = [CLOCK])]
     fn sample() {
 
-        resources.IR.read_pin_state();
+        *resources.CLOCK += 1;
 
-        if resources.IR.done {
-            // spawn debug_print task
-            spawn.debug_print().unwrap();
-        }
+        //resources.IR.read_pin_state();
 
         schedule
             .sample(scheduled + 4200.cycles())
@@ -114,9 +115,25 @@ const APP: () = {
     }
 
 
-    #[interrupt(binds = EXTI9_5, resources = [EXTI, IR, LED])]
+    #[interrupt(binds = EXTI9_5, resources = [EXTI, INPIN, LED, CLOCK, IRRECV])]
     fn on_ir_recv() {
-        resources.IR.get_pin_ref().clear_interrupt_pending_bit(&mut resources.EXTI);
+
+        let level = resources.INPIN.is_low();
+        // assert!(this != prev level)
+        resources.INPIN.clear_interrupt_pending_bit(&mut resources.EXTI);
+
+        let mut recv = resources.IRRECV;
+        let mut clk = resources.CLOCK;
+
+        if recv.is_init() {
+            *clk = 0;
+        }
+
+        let time = *clk;
+
+        // Add event from IR PIN
+        recv.log_event(time, level);
+
         resources.LED.toggle();
     }
 
@@ -132,3 +149,53 @@ const APP: () = {
 
 };
 
+#[derive(PartialEq)]
+pub enum State {
+    Init,
+    Capture,
+
+    Done,
+}
+
+
+const BUF_LEN: usize = 32;
+
+pub struct IrRecv {
+    pub buf: [u32; BUF_LEN],
+    pub cur: usize,
+    pub state: State,
+}
+
+impl IrRecv {
+
+    pub fn new() -> Self {
+        Self {
+            buf: [0; BUF_LEN],
+            cur: 0,
+            state: State::Init,
+        }
+    }
+
+    /// Feed the state machine
+    pub fn log_event(&mut self, time: u32, _level: bool) {
+
+        match self.state {
+            State::Init => {
+                self.state = State::Capture;
+            },
+            _ => (),
+        };
+
+        if self.cur >= BUF_LEN {
+            self.state = State::Done;
+            return;
+        }
+
+        self.buf[self.cur] = time;
+        self.cur += 1;
+    }
+
+    pub fn is_init(&self) -> bool {
+        self.state == State::Init
+    }
+}
